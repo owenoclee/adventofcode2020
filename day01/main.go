56f2e17211622b432bca7deb79ff14ccf85257b9
Owen Oclee <owenoclee@users.noreply.github.com>
2020-12-11 20:13:29 +0000

    Generalise day01 to work for part 2

--- day01/main.go
+++ day01/main.go
@@ -1,37 +1,72 @@
+// Example usage: cat input.txt | go run . -s 2
 package main
 
 import (
 	"bufio"
+	"errors"
+	"flag"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 )
 
-const desiredNumber = 2020
+const goal = 2020
 
 func main() {
-	availableNumbers := make(map[int]bool)
+	summandsCount := flag.Int("s", 2, "Specify how many summands to use")
+	flag.Parse()
+
+	var expenseReport []int
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		num, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			panic(err)
 		}
-		availableNumbers[num] = true
+		expenseReport = append(expenseReport, num)
 	}
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
 
-	for currentNumber := range availableNumbers {
-		neededNumber := desiredNumber - currentNumber
-		if neededNumber == currentNumber {
-			continue
+	solution, err := solve(expenseReport, *summandsCount)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(solution)
+}
+
+func solve(expenseReport []int, summandsCount int) (int, error) {
+	divisors := make([]int, summandsCount)
+	for col := 0; col < summandsCount; col++ {
+		divisors[col] = int(math.Pow(float64(len(expenseReport)), float64(col)))
+	}
+
+	combinations := int(math.Pow(float64(len(expenseReport)), float64(summandsCount)))
+	for combo := 0; combo < combinations; {
+	Start:
+		combo++
+		indexes := make(map[int]bool)
+		for col := 0; col < summandsCount; col++ {
+			idx := (combo / divisors[col]) % len(expenseReport)
+			if indexes[idx] {
+				goto Start
+			}
+			indexes[idx] = true
 		}
-		if availableNumbers[neededNumber] {
-			fmt.Println(currentNumber * neededNumber)
-			return
+
+		var total int
+		for i := range indexes {
+			total += expenseReport[i]
+		}
+		if total == goal {
+			solution := 1
+			for i := range indexes {
+				solution *= expenseReport[i]
+			}
+			return solution, nil
 		}
 	}
-	fmt.Println("no solution")
+	return 0, errors.New("no solution")
 }
