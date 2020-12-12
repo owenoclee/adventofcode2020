67d1f29423233f17d70f1afd71315ed0f6a56726
Owen Oclee <owenoclee@users.noreply.github.com>
2020-12-12 15:29:28 +0000

    Solve day 3 part 2

--- day03/main.go
+++ day03/main.go
@@ -1,19 +1,48 @@
+// Example usage: cat input.txt | go run . 1:1 3:1 5:1 7:1 1:2
 package main
 
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/owenoclee/adventofcode2020/parse"
 )
 
-const (
-	right      = 3
-	down       = 1
-	tree  byte = '#'
-)
+const tree byte = '#'
+
+type slope struct {
+	right int
+	down  int
+}
 
 func main() {
+	slopeArgs := []string{"3:1"}
+	if len(os.Args) > 1 {
+		slopeArgs = os.Args[1:]
+	}
+	var slopes []slope
+	for _, s := range slopeArgs {
+		parts := strings.Split(s, ":")
+		if len(parts) != 2 {
+			panic(invalidSlopeError(s))
+		}
+		right, err := strconv.Atoi(parts[0])
+		if err != nil {
+			panic(invalidSlopeError(s))
+		}
+		down, err := strconv.Atoi(parts[1])
+		if err != nil {
+			panic(invalidSlopeError(s))
+		}
+
+		slopes = append(slopes, slope{
+			right: right,
+			down:  down,
+		})
+	}
+
 	lines, err := parse.LinesFrom(os.Stdin)
 	if err != nil {
 		panic(err)
@@ -24,13 +53,23 @@ func main() {
 	}
 	width := len(lines[0])
 
-	var treesEncountered int
-	x := right
-	for y := down; y < lineCount; y += down {
-		if lines[y][x%width] == tree {
-			treesEncountered++
+	treesEncountered := make([]int, len(slopes))
+	for i, s := range slopes {
+		x := s.right
+		for y := s.down; y < lineCount; y += s.down {
+			if lines[y][x%width] == tree {
+				treesEncountered[i]++
+			}
+			x += s.right
 		}
-		x += right
 	}
-	fmt.Println(treesEncountered)
+	solution := 1
+	for _, trees := range treesEncountered {
+		solution *= trees
+	}
+	fmt.Println(solution)
+}
+
+func invalidSlopeError(s string) error {
+	return fmt.Errorf("invalid slope: %s", s)
 }
