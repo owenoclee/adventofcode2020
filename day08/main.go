1f3b0344882ff30ca4ab62d250fbd2225bfe059b
Owen Oclee <owenoclee@users.noreply.github.com>
2020-12-23 20:09:51 +0000

    Refactor day 8 to make part 2 easier

--- day08/main.go
+++ day08/main.go
@@ -10,37 +10,61 @@ import (
 	"github.com/owenoclee/adventofcode2020/parse"
 )
 
+type state struct {
+	index       int
+	accumulator int
+}
+
 func main() {
 	ops, err := parse.LinesFrom(os.Stdin)
 	if err != nil {
 		panic(err)
 	}
 
+	stateStack := make(map[int]state)
 	visitedIndexes := make(map[int]struct{})
-	accumulator := 0
-	for i := 0; i < len(ops) && i >= 0; {
-		if _, visitedBefore := visitedIndexes[i]; visitedBefore {
+	for {
+		lastState := state{}
+		if len(stateStack) != 0 {
+			lastState = stateStack[len(stateStack)-1]
+		}
+		if _, visitedBefore := visitedIndexes[lastState.index]; visitedBefore {
 			break
 		}
-		visitedIndexes[i] = struct{}{}
-		opArg := strings.Split(ops[i], " ")
+		visitedIndexes[lastState.index] = struct{}{}
+		opArg := strings.Split(ops[lastState.index], " ")
 		if len(opArg) < 2 {
-			log.Fatalf("error on line %d: line does not satisfy format `<op> <arg>`", i+1)
+			log.Fatalf("error on line %d: line does not satisfy format `<op> <arg>`", lastState.index+1)
 		}
 		op, sarg := opArg[0], opArg[1]
 		arg, err := strconv.Atoi(sarg)
 		if err != nil {
-			log.Fatalf("error on line %d: cannot convert arg to int", i+1)
+			log.Fatalf("error on line %d: cannot convert arg to int", lastState.index+1)
 		}
-		switch op {
-		case "acc":
-			accumulator += arg
-			i++
-		case "jmp":
-			i += arg
-		case "nop":
-			i++
-		}
+		stateStack[len(stateStack)] = nextState(stateStack[len(stateStack)-1], lastState.index, op, arg)
 	}
-	fmt.Println(accumulator)
+	fmt.Println(stateStack[len(stateStack)-1].accumulator)
+}
+
+func nextState(lastState state, index int, op string, arg int) state {
+	accumulator := lastState.accumulator
+	switch op {
+	case "acc":
+		return state{
+			accumulator: accumulator + arg,
+			index:       index + 1,
+		}
+	case "jmp":
+		return state{
+			accumulator: accumulator,
+			index:       index + arg,
+		}
+	case "nop":
+		return state{
+			accumulator: accumulator,
+			index:       index + 1,
+		}
+	default:
+		panic(fmt.Errorf("invalid operation '%s'", op))
+	}
 }
