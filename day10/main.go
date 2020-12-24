9a07eade536526c1699c7aa1166196ed0b39b2bf
Owen Oclee <owenoclee@users.noreply.github.com>
2020-12-24 15:58:32 +0000

    Solve day 10 part 2

--- day10/main.go
+++ day10/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"os"
 	"sort"
 
@@ -9,6 +10,9 @@ import (
 )
 
 func main() {
+	part := flag.Int("p", 1, "Specify which part of the puzzle to solve")
+	flag.Parse()
+
 	adapters, err := parse.IntsFrom(os.Stdin)
 	if err != nil {
 		out.Fatalf("error parsing ints: %v", err)
@@ -22,9 +26,25 @@ func main() {
 	adapters = append(adapters, 0, highest+3)
 
 	sort.Ints(adapters)
-	differences := make(map[int]int)
-	for i := 1; i < len(adapters); i++ {
-		differences[adapters[i]-adapters[i-1]]++
+	if *part != 2 {
+		differences := make(map[int]int)
+		for i := 1; i < len(adapters); i++ {
+			differences[adapters[i]-adapters[i-1]]++
+		}
+		out.Fatalln(differences[1] * differences[3])
 	}
-	out.Fatalln(differences[1] * differences[3])
+
+	paths := make([]int64, len(adapters))
+	paths[len(adapters)-1] = 1
+	for i := len(adapters) - 2; i >= 0; i-- {
+		var p int64
+		for ii := i + 1; ii <= i+3 && ii < len(adapters); ii++ {
+			if adapters[ii] > adapters[i]+3 {
+				break
+			}
+			p += paths[ii]
+		}
+		paths[i] = p
+	}
+	out.Fatalln(paths[0])
 }
