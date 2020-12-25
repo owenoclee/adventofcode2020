6c2af15d4d8e7a4f7a85de35dee1aca51db04c8c
Owen Oclee <owenoclee@users.noreply.github.com>
2020-12-25 21:57:21 +0000

    Solve day 11 part 2

--- day11/main.go
+++ day11/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"math"
 	"os"
 	"strings"
 
@@ -15,6 +17,13 @@ const (
 )
 
 func main() {
+	part := flag.Int("p", 1, "Specify which part of the puzzle to solve")
+	flag.Parse()
+	lookDistance := math.MaxInt32
+	if *part != 2 {
+		lookDistance = 1
+	}
+
 	curLayout, err := parse.LinesFrom(os.Stdin)
 	if err != nil {
 		out.Fatalf("error parsing lines: %v", err)
@@ -22,15 +31,12 @@ func main() {
 	if len(curLayout) == 0 {
 		out.Fatalln("no lines in input")
 	}
-	width := len(curLayout[0])
 	height := len(curLayout)
+	width := len(curLayout[0])
 
-	var lastLayout []string
+	lastLayout := make([]string, len(curLayout))
 	for {
-		lastLayout = make([]string, len(curLayout))
-		for i, l := range curLayout {
-			lastLayout[i] = l
-		}
+		copy(lastLayout, curLayout)
 		curLayout = make([]string, len(curLayout))
 		for row := 0; row < height; row++ {
 			for col := 0; col < width; col++ {
@@ -38,28 +44,19 @@ func main() {
 					curLayout[row] += string(floor)
 					continue
 				}
-				occupiedCount := 0
-				// look at adjacent positions
-				for aRow := row - 1; aRow <= row+1; aRow++ {
-					for aCol := col - 1; aCol <= col+1; aCol++ {
-						isWithinBounds := aRow >= 0 && aRow < height && aCol >= 0 && aCol < width
-						if !(aRow == row && aCol == col) && isWithinBounds {
-							if lastLayout[aRow][aCol] == occupied {
-								occupiedCount++
-							}
-						}
-					}
-				}
+				occupiedCount := nearbyOccupiedCount(lastLayout, row, col, lookDistance)
 				switch {
 				case occupiedCount == 0:
 					curLayout[row] += string(occupied)
-				case occupiedCount >= 4:
+				case occupiedCount >= 5 || (*part != 2 && occupiedCount >= 4):
 					curLayout[row] += string(empty)
 				default:
 					curLayout[row] += string(lastLayout[row][col])
 				}
 			}
 		}
+		out.Println(layoutToString(lastLayout))
+		out.Println()
 		if layoutToString(curLayout) == layoutToString(lastLayout) {
 			break
 		}
@@ -69,4 +66,44 @@ func main() {
 
 func layoutToString(l []string) string {
 	return strings.Join(l, "\n")
+}
+
+func nearbyOccupiedCount(layout []string, row, col, maxDistance int) int {
+	if maxDistance == 0 || !isInBounds(layout, row, col) {
+		return 0
+	}
+
+	var unitVectors [8][2]int
+	i := 0
+	for rRow := -1; rRow <= 1; rRow++ {
+		for rCol := -1; rCol <= 1; rCol++ {
+			if rRow == 0 && rCol == 0 {
+				continue
+			}
+			unitVectors[i] = [2]int{rRow, rCol}
+			i++
+		}
+	}
+
+	occupiedCount := 0
+	for _, v := range unitVectors {
+		for d := 1; d <= maxDistance; d++ {
+			aRow := row + (v[0] * d)
+			aCol := col + (v[1] * d)
+			if !isInBounds(layout, aRow, aCol) {
+				break
+			}
+			if curElem := layout[aRow][aCol]; curElem != floor {
+				if curElem == occupied {
+					occupiedCount++
+				}
+				break
+			}
+		}
+	}
+	return occupiedCount
+}
+
+func isInBounds(layout []string, row, col int) bool {
+	return row >= 0 && col >= 0 && row < len(layout) && col < len(layout[row])
 }
